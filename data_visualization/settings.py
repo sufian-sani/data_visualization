3f2d9f07d7711b8baeb7009048087ade28f4a262
Abu Sufian <abu7suffian@gmail.com>
2022-03-16 14:46:00 +0600

    forth

--- data_visualization/settings.py
+++ data_visualization/settings.py
@@ -84,11 +84,23 @@ WSGI_APPLICATION = 'data_visualization.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '5432',
     }
+
 }
 
 
